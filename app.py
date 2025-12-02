c6aea09a9b68cc032971ecf1b8d54aca5c7231ac
Asghar-123 <160334138+Asghar-123@users.noreply.github.com>
2025-12-02 15:37:53 +0500

    Update base URL for OpenAIChatCompletionsModel

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ api_key = st.secrets["GEMINI_API_KEY"]
 model = OpenAIChatCompletionsModel(
     model="gemini-1.5-flash",
     api_key=api_key,
-    base_url="https://generativelanguage.googleapis.com/v1beta/"
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
 SESSION_DIR = "sessions"
